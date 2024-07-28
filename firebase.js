5169bfc29aa20cff93eb58064ebdda5982f5010d
ADMINISTRAT0R-KING <148674689+ADMINISTRAT0R-KING@users.noreply.github.com>
2024-07-28 20:35:00 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -38,7 +38,7 @@
           get(ref(Database,USER)).then((snapshot)=>{
             if(snapshot){
               alert('Updated Successfuly (•‿•)');
-              window.location.href='./msf/index.html';
+              window.location.href='./MSF/index.html';
             }
           });
         }
@@ -56,7 +56,7 @@
         
         get(ref(Database,USER)).then((snapshot)=>{
           if(snapshot){
-            window.location.href='./msf/index.html';
+            window.location.href='./MSF/index.html';
           }
         });
         
@@ -64,7 +64,7 @@
     }
   }else{
     if(!localStorage.getItem('USER')){
-      window.location.href='./msf/login.html';
+      window.location.href='./MSF/login.html';
     }
     var i=1;
     onChildAdded(ref(Database),(snapshot)=>{
